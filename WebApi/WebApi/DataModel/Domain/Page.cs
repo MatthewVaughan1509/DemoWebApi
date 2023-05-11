7ecc1c46e59b9857a77ff142b92907038a7290a5
Vaughan,Matthew <VAUGHAM2@airproducts.com>
2023-05-11 16:54:03 +0100

    New class

---
+++ WebApi/WebApi/DataModel/Domain/Page.cs
@@ -0,0 +1,13 @@
+﻿namespace DataModel.Domain
+{
+    public class Page
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? HtmlText { get; set; }
+        public string? InsertedBy { get; set; }
+        public DateTime InsertedOn { get; set; }
+        public string? ModifiedBy { get; set; }
+        public DateTime? ModifiedOn { get; set; }
+    }
+}
